0263f048634ccfbed1a35e4d547e17b65831ab3d
kdoherty <kevin_doherty@intuit.com>
2014-04-28 22:32:57 -0700

    Changed orientation to portrait only

--- AndroidChess/src/com/kdoherty/androidchess/MainActivity.java
+++ AndroidChess/src/com/kdoherty/androidchess/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kdoherty.androidchess;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Menu;
 
@@ -16,6 +17,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		setContentView(R.layout.activity_main);
 		initBoard();
 	}
