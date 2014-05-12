4a534e75f3dffc7d9fb134d77abb46374c1f26b9
kdoherty <kevin_doherty@intuit.com>
2014-05-12 09:09:12 -0700

    Added invarient check in constructor

--- Chess/src/com/kdoherty/chess/Square.java
+++ Chess/src/com/kdoherty/chess/Square.java
@@ -22,6 +22,10 @@ public final class Square {
 	 *            Sets this squares column to this value
 	 */
 	public Square(int row, int col) {
+		if (!Board.isInbounds(row, col)) {
+			throw new IllegalArgumentException("Square is not inbounds! Row: "
+					+ row + " col: " + col);
+		}
 		this.row = row;
 		this.col = col;
 	}
@@ -160,7 +164,7 @@ public final class Square {
 		s += (8 - row);
 		return s;
 	}
-	
+
 	public int toNum() {
 		return row * 8 + col;
 	}
