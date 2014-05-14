a58e9ed1483aafcb5e2e5cb2ace8a9819f2c9fba
kdoherty <kevin_doherty@intuit.com>
2014-05-14 10:39:10 -0700

    Changed central square bonus

--- Chess/src/com/kdoherty/engine/KnightEval.java
+++ Chess/src/com/kdoherty/engine/KnightEval.java
@@ -10,17 +10,19 @@ public class KnightEval {
 	private static int START_VALUE = 325;
 	private static int ENDGAME_PENALTY = -10;
 	private static int BLOCKING_CNTR_PAWN_PENALTY = -10;
-	private static int[] BOARD_VALUES = { -50, -40, -30, -30, -30, -30,
-			-40, -50, -40, -20, 0, 5, 5, 0, -20, -40, -30, 5, 10, 15, 15,
-			10, 5, -30, -30, 0, 15, 20, 20, 15, 0, -30, -30, 5, 15, 20, 20,
-			15, 5, -30, -30, 0, 10, 15, 15, 10, 0, -30, -40, -20, 0, 0, 0,
-			0, -20, -40, -50, -40, -30, -30, -30, -30, -40, -50 };
+	private static int[] BOARD_VALUES = { -50, -40, -30, -30, -30, -30, -40, -50,
+										  -40, -20,   0,   5,   5,   0, -20, -40,
+										  -30,   5,  10,  12,  12,  10,   5, -30,
+										  -30,   0,  15,  15,  15,  15,   0, -30,
+										  -30,   5,  15,  15,  15,  15,   5, -30,
+										  -30,   0,  10,  12,  12,  10,   0, -30,
+										  -40, -20,   0,   0,   0,   0, -20, -40,
+										  -50, -40, -30, -30, -30, -30, -40, -50 };
 
 	public static int eval(Board b, Knight n) {
 		int value = START_VALUE;
 		Square s = n.getSq();
-		int index = n.getColor() == Color.WHITE ? s.toNum() : 63 - s
-				.toNum();
+		int index = n.getColor() == Color.WHITE ? s.toNum() : 63 - s.toNum();
 		if (Evaluate.isEndGame(b))
 			value += ENDGAME_PENALTY;
 		value += BOARD_VALUES[index];
