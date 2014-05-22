13d43aeaddb4d18749f778a940b5a23984e18e69
kdoherty <kevin_doherty@intuit.com>
2014-05-21 21:20:36 -0700

    Added javadocs and changed visibility of methods

--- Chess/src/com/kdoherty/android/PieceImageView.java
+++ Chess/src/com/kdoherty/android/PieceImageView.java
@@ -4,36 +4,68 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-public class PieceImageView extends ImageView {
-	
+/**
+ * This class is an ImageView which can keep track of its position on a grid
+ * view.
+ * 
+ * @author Kevin Doherty
+ * 
+ */
+class PieceImageView extends ImageView {
+
+	/** The row this is located on */
 	private int row;
+
+	/** The column this is located on */
 	private int col;
-	
-	public PieceImageView(Context context) {
+
+	PieceImageView(Context context) {
 		super(context);
 	}
-	
-	public PieceImageView(Context context, AttributeSet attrs) {
+
+	PieceImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
 
-	public PieceImageView(Context context, AttributeSet attrs, int defStyle) {
+	PieceImageView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
-	
-	public void setRow(int row) {
+
+	/**
+	 * Sets the row of this to the input row.
+	 * 
+	 * @param row
+	 *            The row to set this row to
+	 */
+	void setRow(int row) {
 		this.row = row;
 	}
-	
-	public void setCol(int col) {
+
+	/**
+	 * Sets the column of this to the input column.
+	 * 
+	 * @param col
+	 *            The column to set this column to
+	 */
+	void setCol(int col) {
 		this.col = col;
 	}
 
-	public int getRow() {
+	/**
+	 * Gets the row of the Board that this image is located on
+	 * 
+	 * @return The row of the Board that this image is located on
+	 */
+	int getRow() {
 		return row;
 	}
 
-	public int getCol() {
+	/**
+	 * Gets the column of the Board that this image is located on
+	 * 
+	 * @return The column of the Board that this image is located on
+	 */
+	int getCol() {
 		return col;
 	}
 }
