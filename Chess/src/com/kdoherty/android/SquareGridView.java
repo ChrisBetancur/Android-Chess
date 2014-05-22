b279f04c89c81350279dbec690112e347a083bfd
kdoherty <kevin_doherty@intuit.com>
2014-05-21 21:34:54 -0700

    Added javadocs

--- Chess/src/com/kdoherty/android/SquareGridView.java
+++ Chess/src/com/kdoherty/android/SquareGridView.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.GridView;
 
-public class SquareGridView extends GridView {
+/**
+ * This class ensures that the Squares in the Board view are always Square
+ * @author Kevin Doherty
+ *
+ */
+class SquareGridView extends GridView {
 
 	public SquareGridView(final Context context) {
 		super(context);
@@ -19,14 +24,20 @@ public class SquareGridView extends GridView {
 		super(context, attrs, defStyle);
 	}
 
-	@Override
-	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+	/** 
+	 * Only pass widthMeasureSpc to GridView's onMeasure method so that
+	 * it stays Square.
+	 */
+	@Override public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, widthMeasureSpec);
 	}
 
-	@Override
-	protected void onSizeChanged(final int w, final int h, final int oldw,
-			final int oldh) {
+	/**
+	 * Only pass width to GridView's onSizeChanged method so that
+	 * it stays Square.
+	 */
+	@Override protected void onSizeChanged(final int w, final int h,
+			final int oldw, final int oldh) {
 		super.onSizeChanged(w, w, oldw, oldh);
 	}
 }
