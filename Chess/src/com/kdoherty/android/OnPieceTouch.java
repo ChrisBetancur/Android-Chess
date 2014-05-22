a9b2730cf6386907c00945b15ef5f3bc6f52e5b7
kdoherty <kevin_doherty@intuit.com>
2014-05-21 21:05:28 -0700

    Added javadocs

--- Chess/src/com/kdoherty/android/OnPieceTouch.java
+++ Chess/src/com/kdoherty/android/OnPieceTouch.java
@@ -6,10 +6,22 @@ import android.view.View;
 import android.view.View.DragShadowBuilder;
 import android.view.View.OnTouchListener;
 
-public enum OnPieceTouch implements OnTouchListener {
-	
+/**
+ * This is responsible for starting to drag a Piece once it is touched.
+ * Only one instance of this Class should ever be needed.
+ * 
+ * @author Kevin Doherty
+ * 
+ */
+enum OnPieceTouch implements OnTouchListener {
+
 	INSTANCE;
 
+	/**
+	 * Starts the dragging of a Piece. As soon as dragging is started, the Piece
+	 * itself becomes invisible on its current Square, but a shadow of the Piece
+	 * is seen where the Piece is being dragged.
+	 */
 	@Override
 	public boolean onTouch(View view, MotionEvent event) {
 		int action = event.getAction();
