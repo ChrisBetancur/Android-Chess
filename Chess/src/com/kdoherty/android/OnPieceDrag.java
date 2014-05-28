6dd12360b45f229120c4c079e3c7506c92c503c0
kdoherty <kevin_doherty@intuit.com>
2014-05-27 21:24:22 -0700

    Used passTurn method

--- Chess/src/com/kdoherty/android/OnPieceDrag.java
+++ Chess/src/com/kdoherty/android/OnPieceDrag.java
@@ -59,8 +59,7 @@ class OnPieceDrag implements OnDragListener {
 	public boolean onDrag(View v, DragEvent event) {
 		int action = event.getAction();
 		PieceImageView view = (PieceImageView) event.getLocalState();
-		int startingRow = view.getRow();
-		int startingCol = view.getCol();
+
 		switch (action) {
 		case DragEvent.ACTION_DRAG_STARTED:
 			break;
@@ -71,40 +70,31 @@ class OnPieceDrag implements OnDragListener {
 		case DragEvent.ACTION_DRAG_ENDED:
 			view.setVisibility(View.VISIBLE);
 			if (moved) {
-				board.toggleSideToMove();
-				context.toggleTimer();
 				context.makeCpuMove();
 			}
 			break;
 		case DragEvent.ACTION_DROP:
-			Piece piece = board.getOccupant(startingRow, startingCol);
+			Piece piece = board.getOccupant(view.getRow(), view.getCol());
+			if (piece == null) {
+				view.setVisibility(View.VISIBLE);
+				break;
+			}
 
 			if (piece.getColor() == board.getSideToMove()
 					&& piece.canMove(board, targetRow, targetCol)) {
 
-				piece.moveTo(board, targetRow, targetCol);
-
-				// Handles Pawn Promotion
-				if (piece instanceof Pawn) {
-					Pawn pawn = (Pawn) piece;
-					if (pawn.isPromoting()) {
-						Piece promotedTo = context.askPromotion(piece
-								.getColor());
-						board.setPiece(targetRow, targetCol, promotedTo);
-					}
+				// Handle Pawn Promotion
+				if (piece instanceof Pawn && ((Pawn) piece).isPromoting()) {
+					Piece promotedTo = context.askPromotion(piece.getColor());
+					board.setPiece(targetRow, targetCol, promotedTo);
 				}
-
-				board.addMove(new Move(piece, targetRow, targetCol));
-				if (board.isGameOver()) {
-					context.showGameOver();
-				}
-				context.refreshAdapter(board);
-
+				//TODO: Not passing type here. This will break add to taken Pieces for enPoissant
+				// and undoing of all non-normal moves
+				context.passTurn(new Move(piece, targetRow, targetCol));
 				moved = true;
-
 				return true;
 			} else {
-				// Piece could not move
+				// Piece could not move to the Square it was dropped on
 				view.setVisibility(View.VISIBLE);
 			}
 			break;
